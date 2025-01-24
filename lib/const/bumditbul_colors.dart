f880f257e4c077d9d31cb6648a78bf2578e18bb6
jiyun040 <annajungarmy@gmail.com>
2025-01-24 16:46:04 +0900

    📝 :: [#2] 색상 추가

---
+++ lib/const/bumditbul_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BumditbulColors{
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black50 = const Color(0xFFFAFAFA);
+  static Color black100 = const Color(0xFFF5F5F5);
+  static Color black200 = const Color(0xFFEEEEEE);
+  static Color black300 = const Color(0xFFE0E0E0);
+  static Color black400 = const Color(0xFFBDBDBD);
+  static Color black500 = const Color(0xFF9E9E9E);
+  static Color black600 = const Color(0xFF757575);
+  static Color black700 = const Color(0xFF616161);
+  static Color black8000 = const Color(0xFF424242);
+  static Color black900 = const Color(0xFF212121);
+  static Color black = const Color(0xFF000000);
+
+  static Color green50 = const Color(0xFFE8F6E9);
+  static Color green100 = const Color(0xFFC8E6C9);
+  static Color green200 = const Color(0xFFA5D6A7);
+  static Color green300 = const Color(0xFF81C784);
+  static Color green400 = const Color(0xFF66BB6A);
+  static Color green500 = const Color(0xFF4CAF50);
+  static Color green600 = const Color(0xFF43A047);
+  static Color green700 = const Color(0xFF388E3C);
+  static Color green800 = const Color(0xFF2E7D32);
+  static Color green900 = const Color(0xFF1B5E20);
+}
