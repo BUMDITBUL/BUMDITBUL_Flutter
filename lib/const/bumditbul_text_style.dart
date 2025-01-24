93929c398477817276e5edacee37a132ff214ade
jiyun040 <annajungarmy@gmail.com>
2025-01-24 16:46:17 +0900

    📝 :: [#2] 텍스트 스타일 추가

---
+++ lib/const/bumditbul_text_style.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:bumditbul_flutter/const/bumditbul_colors.dart';
+
+class BumditbulTextStyle{
+  static TextStyle JoinLogin({required Color color}){
+    return TextStyle(
+      color: BumditbulColors.white,
+      fontSize: 15,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w600,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+  static TextStyle HintTxt({required Color color}){
+    return TextStyle(
+      color: BumditbulColors.black300,
+      fontSize: 12,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w400,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+  static TextStyle ErrorMsg({required Color color}){
+    return TextStyle(
+      color: BumditbulColors.green400,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w400,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+  static TextStyle BarLogo({required Color color}){
+    return TextStyle(
+      color: BumditbulColors.green400,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+}
