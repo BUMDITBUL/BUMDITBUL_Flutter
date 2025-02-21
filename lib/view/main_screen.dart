2cb26cc84628c629d05946275fe50901b4c571c1
jiyun040 <annajungarmy@gmail.com>
2025-02-21 15:21:41 +0900

    📝 :: [#4] 메인화면 배너, 앱바, 뷰 추가

---
+++ lib/view/main_screen.dart
@@ -0,0 +1,21 @@
+import 'package:bumditbul_flutter/component/bumditbul_appbar.dart';
+import 'package:bumditbul_flutter/component/main_screen_banner.dart';
+import 'package:bumditbul_flutter/const/bumditbul_colors.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const BumditbulAppbar(),
+      backgroundColor: BumditbulColors.black900,
+      body: Column(
+        children: [
+          MainScreenBanner(),
+        ],
+      ),
+    );
+  }
+}
