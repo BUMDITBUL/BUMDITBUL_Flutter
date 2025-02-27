52204699146d2c6665f9af3cd2e04aad41126a68
jiyun040 <annajungarmy@gmail.com>
2025-02-27 18:54:31 +0900

    📝 :: [#4] 색상, 텍스트 스타일 수정

--- lib/const/bumditbul_text_style.dart
+++ lib/const/bumditbul_text_style.dart
@@ -23,6 +23,7 @@ class BumditbulTextStyle{
   static TextStyle ErrorMsg({required Color color}){
     return TextStyle(
       color: BumditbulColors.green400,
+      fontSize: 8,
       fontFamily: 'Inter',
       fontWeight: FontWeight.w400,
       overflow: TextOverflow.ellipsis,
@@ -31,6 +32,7 @@ class BumditbulTextStyle{
   static TextStyle BarLogo({required Color color}){
     return TextStyle(
       color: BumditbulColors.green400,
+      fontSize: 12,
       fontFamily: 'Inter',
       fontWeight: FontWeight.w500,
       overflow: TextOverflow.ellipsis,
@@ -39,6 +41,7 @@ class BumditbulTextStyle{
   static TextStyle BannerTxtWhite({required Color color}){
     return TextStyle(
       color: BumditbulColors.white,
+      fontSize: 16,
       fontFamily: 'Inter',
       fontWeight: FontWeight.w700,
       overflow: TextOverflow.ellipsis,
@@ -47,6 +50,7 @@ class BumditbulTextStyle{
   static TextStyle BannerTxtGreen({required Color color}){
     return TextStyle(
       color: BumditbulColors.green400,
+      fontSize: 16,
       fontFamily: 'Inter',
       fontWeight: FontWeight.w700,
       overflow: TextOverflow.ellipsis,
@@ -55,8 +59,36 @@ class BumditbulTextStyle{
   static TextStyle MainDate({required Color color}){
     return TextStyle(
       color: BumditbulColors.white,
+      fontSize: 15,
       fontFamily: 'Inter',
       fontWeight: FontWeight.w600,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+  static TextStyle TaskName({required Color color}){
+    return TextStyle(
+      color: BumditbulColors.white,
+      fontSize: 13,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w600,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+  static TextStyle TaskTime({required Color color}){
+    return TextStyle(
+      color: BumditbulColors.white,
+      fontSize: 8,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+  static TextStyle TaskRange({required Color color}){
+    return TextStyle(
+      color: BumditbulColors.white,
+      fontSize: 10,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
       overflow: TextOverflow.ellipsis,
     );
   }
